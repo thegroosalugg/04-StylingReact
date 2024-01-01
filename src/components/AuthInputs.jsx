3640b57687622028615199946a29210171bfd51c
victor loginov <tkoe88@zoho.com>
2024-01-01 20:08:52 +0100

    refactored styled components into reusable components

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { styled } from "styled-components";
+import Button from "./Button";
+import Input from "./CustomInput"; // the name can be changed because it is the exported DEFAULT function
 
 // styled is a JS object and we access its properties
 const ControlsDiv = styled.div`
@@ -7,43 +9,6 @@ const ControlsDiv = styled.div`
   flex-direction: column;
   gap: 0.5rem;
   margin-bottom: 1.5rem;
-`;
-
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${({ $invalid }) => ($invalid ? "#f87171" : "#6b7280")};
-`; // dynamically sets color by passing a prop as a function
-   // adding $ infront of prop names ensures there are no clashes between in-built props
-
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${({ $invalid }) => ($invalid ? "#fed2d2" : "#d1d5db")};
-  color: ${({ $invalid }) => ($invalid ? "#ef4444" : "#374151")};
-  border: 1px solid ${({ $invalid }) => ($invalid ? "#f73f3f" : "transparent")};
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`;
-
-const Button = styled.button`
-  padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  border-radius: 0.25rem;
-  color: #1f2937;
-  background-color: #f0b322;
-  border-radius: 6px;
-  border: none;
-
-  &:hover {
-    background-color: #f0920e;
-  }
 `;
 
 export default function AuthInputs() {
@@ -69,24 +34,20 @@ export default function AuthInputs() {
   return (
     <div id="auth-inputs">
       <ControlsDiv>
-        <p>
-          <Label $invalid={emailNotValid}>Email</Label>
-          <Input
-            type="email"
-            $invalid={emailNotValid}
-            onChange={(event) => handleInputChange("email", event.target.value)}
-          />
-        </p>
-        <p>
-          <Label $invalid={passwordNotValid}> Password</Label>
-          <Input
-            type="password"
-            $invalid={passwordNotValid}
-            onChange={(event) =>
-              handleInputChange("password", event.target.value)
-            }
-          />
-        </p>
+        <Input
+          type="email"
+          label="email"
+          $invalid={emailNotValid}
+          onChange={(event) => handleInputChange("email", event.target.value)}
+        />
+        <Input
+          type="password"
+          label="password"
+          $invalid={passwordNotValid}
+          onChange={(event) =>
+            handleInputChange("password", event.target.value)
+          }
+        />
       </ControlsDiv>
       <div className="actions">
         <button type="button" className="text-button">
