3d9e98989caf7028429c2166a475f5e0deb94402
victor loginov <tkoe88@zoho.com>
2024-01-01 21:43:54 +0100

    header styled with tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@ import logo from "../assets/logo.png";
 
 export default function Header() {
   return (
-    <header>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+    <header className="flex flex-col items-center mt-8 mb-16">
+      <img src={logo} alt="A canvas" className="w-44 h-44 mb-8 object-contain"/>
+      <h1 className="text-4xl text-center font-semibold tracking-widest uppercase text-amber-800 font-custom">ReactArt</h1>
+      <p className="text-stone-500">A community of artists and art-lovers.</p>
     </header>
   );
 }
